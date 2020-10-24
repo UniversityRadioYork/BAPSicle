a6fce02c56d53a61e73b6b102c0be2dbe1e31e3e
Matthew Stratford <hi@mstratford.net>
2020-10-24 16:16:41 +0100

    Add unit test.

---
+++ tests/test_ui.py
@@ -0,0 +1,41 @@
+from bapsicle import server
+import unittest
+
+
+class TestUI(unittest.TestCase):
+
+    # initialization logic for the test suite declared in the test module
+    # code that is executed before all tests in one test run
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    # clean up logic for the test suite declared in the test module
+    # code that is executed after all tests in one test run
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    # initialization logic
+    # code that is executed before each test
+    def setUp(self):
+        self.app = server.app.test_client()
+        self.app.testing = True
+
+    # clean up logic
+    # code that is executed after each test
+    def tearDown(self):
+        pass
+
+    def test_index_status_code(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/')
+
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200)
+
+
+# runs the unit tests in the module
+if __name__ == '__main__':
+    unittest.main()
