d1103e66cf6d5d312161aa7ea973fa18a8f394fb
Matthew Stratford <hi@mstratford.net>
2020-10-26 21:15:28 +0000

    Initial Root logging

---
+++ __init__.py
@@ -0,0 +1,5 @@
+# see
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+logging.basicConfig(filename='bapsicle_log.log', level=logging.INFO)
+logging.info('Started Logging')
